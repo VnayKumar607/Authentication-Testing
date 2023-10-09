7f07fb94f4af458dd05ec03e3cb994a66ffe3872
Vinay kumar <vinayjangid93@gmail.com>
2023-10-09 21:03:27 +0530

    Added Environment vars

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //jshint esversion:6
+require('dotenv').config();
+
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
@@ -6,7 +8,7 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
-
+console.log(process.env.API_KEY);
 app.set('view engine' , 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -19,8 +21,8 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-const secret = "stringnoonecanguessinalifetime";
-userSchema.plugin(encrypt, {secret: secret , encryptedFields: ["password"] });
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ["password"] });
 
 const User = new mongoose.model("User" , userSchema);
 
