bf3f060840c9aeddce475d4397890a345b23ba44
Vinay kumar <vinayjangid93@gmail.com>
2023-10-10 22:26:16 +0530

    Level-6 Added Google oAut2.0

--- app.js
+++ app.js
@@ -10,13 +10,17 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+//Level-6 requiring Google strategy
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
+app.use(express.static("public"));
 app.set('view engine' , 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
+
 
 //Setting up Session and passport
 app.use(session({
@@ -28,25 +32,65 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //CONNECTION
-mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+mongoose.connect("mongodb://127.0.0.1:27017/userDB" ,{useNewUrlParser: true});
+
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
-userSchema.plugin(passportLocalMongoose);
 
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 //userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ["password"] });
 
 const User = new mongoose.model("User" , userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// Serialize the user object
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  // Deserialize the user object
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+      done(null, user);
+    }).catch((err)=>{
+        console.log(err);
+        done(err,null);
+    })
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/" , function(req,res){
     res.render("home");
+});
+
+app.get("/auth/google" , 
+    passport.authenticate('google', { scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets", 
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
 });
 
 app.get("/login" , function(req,res){
