c8c58ee3258910c7951116e61ce31f18eb4744e8
Vinay kumar <vinayjangid93@gmail.com>
2023-10-10 15:02:19 +0530

    Level-4 Added Salting and Hashing

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+
+//Level-4 SALTING AND HASHING
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
-//console.log(process.env.API_KEY);
+
 app.set('view engine' , 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -41,33 +44,39 @@ app.get("/register" , function(req,res){
 
 app.post("/register", function(req,res){
 
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save().then(()=>{
+            res.render("secrets");
+        }).catch((err) =>{
+            console.log(err);
+        })
     });
-    newUser.save().then(()=>{
-        res.render("secrets");
-    }).catch((err) =>{
-        console.log(err);
-    })
-    
 });
 
 app.post("/login", function(req,res){
 
-    const username = req.body.username;
-    const password = md5(req.body.password);
+    const username = req.body.username; 
+    const password = req.body.password;
 
     User.findOne({email: username}).then((foundUser) =>{
         if(foundUser){
-            if(foundUser.password === password){
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if(result === true){
                 res.render("secrets");
-            }
+                }else{
+                    res.send("Wrong password!")
+                }
+            });  
         }
+
     }).catch((err) =>{
         console.log(err);
-    })
-})
+    });
+});
 
 
 
